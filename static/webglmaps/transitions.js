d6b87708c8befee802c99f7d8521437607f27d65
Tom Payne <twpayne@gmail.com>
2012-01-09 20:57:09 +0100

    Add new splat transition

--- static/webglmaps/transitions.js
+++ static/webglmaps/transitions.js
@@ -35,6 +35,17 @@ webglmaps.transitions.pop = function(base, diff, delta) {
  * @param {number} delta Delta.
  * @return {number} Value.
  */
+webglmaps.transitions.splat = function(base, diff, delta) {
+  return base + (1 - Math.cos(delta * Math.PI / 2)) * diff;
+};
+
+
+/**
+ * @param {number} base Base.
+ * @param {number} diff Diff.
+ * @param {number} delta Delta.
+ * @return {number} Value.
+ */
 webglmaps.transitions.swing = function(base, diff, delta) {
   return base + 0.5 * (1 - Math.cos(delta * Math.PI)) * diff;
 };
