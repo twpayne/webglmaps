b6b9a38f2200d7d0af4be19bfcfb2958fdea45e4
Tom Payne <twpayne@gmail.com>
2012-01-09 14:01:30 +0100

    Add shift-drag to rotate

--- static/webglmaps/mousenavigation.js
+++ static/webglmaps/mousenavigation.js
@@ -14,7 +14,8 @@ goog.provide('webglmaps.MouseNavigation');
  */
 webglmaps.MouseNavigationState = {
   NONE: 0,
-  PANNING: 1
+  PANNING: 1,
+  ROTATING: 2
 };
 
 
@@ -32,6 +33,24 @@ webglmaps.MouseNavigation = function() {
    * @type {webglmaps.Map}
    */
   this.map_ = null;
+
+  /**
+   * @private
+   * @type {Element}
+   */
+  this.element_ = null;
+
+  /**
+   * @private
+   * @type {number}
+   */
+  this.initialRotation_ = 0;
+
+  /**
+   * @private
+   * @type {goog.vec.Vec3.Type}
+   */
+  this.previousPixel_ = goog.vec.Vec3.create();
 
   /**
    * @private
@@ -65,13 +84,23 @@ goog.inherits(webglmaps.MouseNavigation, goog.events.EventHandler);
  * @param {goog.events.BrowserEvent} event Event.
  */
 webglmaps.MouseNavigation.prototype.handleMouseDown = function(event) {
-  if (event.isMouseActionButton()) {
-    this.state_ = webglmaps.MouseNavigationState.PANNING;
-    goog.vec.Vec3.setFromValues(
-        this.previousPosition_, event.clientX, event.clientY, 0);
-    this.map_.fromElementPixelToPosition(
-        this.previousPosition_, this.previousPosition_);
+  if (!event.isMouseActionButton()) {
+    return;
   }
+  if (event.shiftKey) {
+    this.state_ = webglmaps.MouseNavigationState.ROTATING;
+    var angle = Math.atan2(
+        this.element_.height / 2 - event.clientY,
+        event.clientX - this.element_.width / 2);
+    this.initialRotation_ = this.map_.getRotation() - angle;
+  } else {
+    this.state_ = webglmaps.MouseNavigationState.PANNING;
+  }
+  event.preventDefault();
+  goog.vec.Vec3.setFromValues(
+      this.previousPixel_, event.clientX, event.clientY, 0);
+  this.map_.fromElementPixelToPosition(
+      this.previousPixel_, this.previousPosition_);
 };
 
 
@@ -79,20 +108,27 @@ webglmaps.MouseNavigation.prototype.handleMouseDown = function(event) {
  * @param {goog.events.BrowserEvent} event Event.
  */
 webglmaps.MouseNavigation.prototype.handleMouseMove = function(event) {
+  if (this.state_ == webglmaps.MouseNavigationState.NONE) {
+    return;
+  }
+  event.preventDefault();
+  var pixel = goog.vec.Vec3.createFromValues(
+      event.clientX, event.clientY, 0);
+  var position = goog.vec.Vec3.create();
+  this.map_.fromElementPixelToPosition(pixel, position);
   if (this.state_ == webglmaps.MouseNavigationState.PANNING) {
-    event.preventDefault();
-    var position = goog.vec.Vec3.createFromValues(
-        event.clientX, event.clientY, 0);
-    this.map_.fromElementPixelToPosition(position, position);
     var center = this.map_.getCenter();
     goog.vec.Vec3.subtract(center, position, center);
     goog.vec.Vec3.add(center, this.previousPosition_, center);
     this.map_.setCenter(center);
-    goog.vec.Vec3.setFromValues(
-        this.previousPosition_, event.clientX, event.clientY, 0);
-    this.map_.fromElementPixelToPosition(
-        this.previousPosition_, this.previousPosition_);
+  } else if (this.state_ == webglmaps.MouseNavigationState.ROTATING) {
+    var angle = Math.atan2(
+        this.element_.height / 2 - event.clientY,
+        event.clientX - this.element_.width / 2);
+    this.map_.setRotation(this.initialRotation_ + angle);
   }
+  goog.vec.Vec3.setFromArray(this.previousPixel_, pixel);
+  this.map_.fromElementPixelToPosition(pixel, this.previousPosition_);
 };
 
 
@@ -142,14 +178,20 @@ webglmaps.MouseNavigation.prototype.handleMouseWheel = function(event) {
 webglmaps.MouseNavigation.prototype.setMap = function(map) {
   this.removeAll();
   this.map_ = map;
-  if (!goog.isNull(this.map_)) {
-    var element = map.getElement();
-    this.listen(element, goog.events.EventType.MOUSEDOWN, this.handleMouseDown);
-    this.listen(element, goog.events.EventType.MOUSEMOVE, this.handleMouseMove);
-    this.listen(element, goog.events.EventType.MOUSEOUT, this.handleMouseOut);
-    this.listen(element, goog.events.EventType.MOUSEUP, this.handleMouseUp);
+  if (goog.isNull(this.map_)) {
+    this.element_ = null;
+  } else {
+    this.element_ = map.getElement();
     this.listen(
-        new goog.events.MouseWheelHandler(element),
+        this.element_, goog.events.EventType.MOUSEDOWN, this.handleMouseDown);
+    this.listen(
+        this.element_, goog.events.EventType.MOUSEMOVE, this.handleMouseMove);
+    this.listen(
+        this.element_, goog.events.EventType.MOUSEOUT, this.handleMouseOut);
+    this.listen(
+        this.element_, goog.events.EventType.MOUSEUP, this.handleMouseUp);
+    this.listen(
+        new goog.events.MouseWheelHandler(this.element_),
         goog.events.MouseWheelHandler.EventType.MOUSEWHEEL,
         this.handleMouseWheel);
   }
