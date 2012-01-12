f0540bf768dddf2ac46723c1b9f5bbc4be15fc62
Tom Payne <twpayne@gmail.com>
2012-01-12 20:02:26 +0100

    Make hue/saturation shader configurable

--- static/webglmaps/shader/fragment/huesaturation.js
+++ static/webglmaps/shader/fragment/huesaturation.js
@@ -1,5 +1,7 @@
 goog.provide('webglmaps.shader.fragment.HueSaturation');
 
+goog.require('goog.math');
+goog.require('webglmaps.Uniform');
 goog.require('webglmaps.shader.Fragment');
 
 
@@ -7,14 +9,30 @@ goog.require('webglmaps.shader.Fragment');
 /**
  * @constructor
  * @extends {webglmaps.shader.Fragment}
+ * @param {number=} opt_hue Hue.
+ * @param {number=} opt_saturation Saturation.
  * @see https://github.com/evanw/glfx.js
  */
-webglmaps.shader.fragment.HueSaturation = function() {
+webglmaps.shader.fragment.HueSaturation = function(opt_hue, opt_saturation) {
+
+  /**
+   * @private
+   * @type {webglmaps.Uniform}
+   */
+  this.hueUniform_ = new webglmaps.Uniform('uHue');
+
+  /**
+   * @private
+   * @type {webglmaps.Uniform}
+   */
+  this.saturationUniform_ = new webglmaps.Uniform('uSaturation');
 
   goog.base(this, [
     'precision mediump float;',
     '',
     'uniform float uAlpha;',
+    'uniform float uHue;',
+    'uniform float uSaturation;',
     'uniform sampler2D uTexture;',
     'uniform float uTime;',
     '',
@@ -23,10 +41,7 @@ webglmaps.shader.fragment.HueSaturation = function() {
     'void main() {',
     '  vec4 color = texture2D(uTexture, vTexCoord);',
     '',
-    '  float hue = 0.0;',
-    '  float saturation = -sin(uTime / 5000.0);',
-    '',
-    '  float angle = hue * 3.14159265;',
+    '  float angle = uHue * 3.14159265;',
     '  float s = sin(angle), c = cos(angle);',
     '  vec3 weights = (vec3(2.0 * c, -sqrt(3.0) * s - c, sqrt(3.0) * s - c)',
     '                  + 1.0) / 3.0;',
@@ -39,16 +54,31 @@ webglmaps.shader.fragment.HueSaturation = function() {
     '  ',
     '  /* saturation adjustment */',
     '  float average = (color.r + color.g + color.b) / 3.0;',
-    '  if (saturation > 0.0) {',
+    '  if (uSaturation > 0.0) {',
     '    color.rgb += (average - color.rgb)',
-    '                 * (1.0 - 1.0 / (1.001 - saturation));',
+    '                 * (1.0 - 1.0 / (1.001 - uSaturation));',
     '  } else {',
-    '    color.rgb += (average - color.rgb) * (-saturation);',
+    '    color.rgb += (average - color.rgb) * (-uSaturation);',
     '  }',
     '',
     '  gl_FragColor = vec4(color.rgb, uAlpha);',
     '}'
-  ].join('\n'));
+  ].join('\n'), [
+    this.hueUniform_,
+    this.saturationUniform_
+  ]);
+
+  /**
+   * @private
+   * @type {number}
+   */
+  this.hue_ = opt_hue || 0;
+
+  /**
+   * @private
+   * @type {number}
+   */
+  this.saturation_ = opt_saturation || 0;
 
 };
 goog.inherits(
@@ -56,8 +86,50 @@ goog.inherits(
 
 
 /**
+ * @return {number} Hue.
+ */
+webglmaps.shader.fragment.HueSaturation.prototype.getHue = function() {
+  return this.hue_;
+};
+
+
+/**
+ * @return {number} Saturation.
+ */
+webglmaps.shader.fragment.HueSaturation.prototype.getSaturation = function() {
+  return this.saturation_;
+};
+
+
+/**
  * @inheritDoc
  */
 webglmaps.shader.fragment.HueSaturation.prototype.isAnimated = function() {
   return true;
+};
+
+
+/**
+ * @param {number} hue Hue.
+ */
+webglmaps.shader.fragment.HueSaturation.prototype.setHue = function(hue) {
+  this.hue_ = hue;
+};
+
+
+/**
+ * @param {number} saturation Saturation.
+ */
+webglmaps.shader.fragment.HueSaturation.prototype.setSaturation =
+    function(saturation) {
+  this.saturation_ = saturation;
+};
+
+
+/**
+ * @inheritDoc
+ */
+webglmaps.shader.fragment.HueSaturation.prototype.setUniforms = function() {
+  this.hueUniform_.set1f(this.hue_);
+  this.saturationUniform_.set1f(this.saturation_);
 };
